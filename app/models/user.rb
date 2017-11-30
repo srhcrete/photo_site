966432a62326fda2a25129c963e7aea6ffa8dc1b
David Hale and Sarah Lindmar <david+sarah@>
2017-11-30 14:53:38 -0800

    add edit user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   validates :username, :presence => true
   validates :username, :uniqueness => true
 
-  validates :password, :presence => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
